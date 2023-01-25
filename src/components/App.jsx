ecd996ad315485435dddcdd398736df473c92ca2
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-01-25 20:03:08 +0100

    vertical nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from "./navigations/Navigation"
+import VerticalNavigation from "./navigations/VerticalNavigation"
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <VerticalNavigation />
     </div>
   );
 }
---
+++ src/components/navigations/VerticalNavigation.jsx
@@ -0,0 +1,45 @@
+import meditationIcon from '../../assets/meditation-icon.svg'
+import swimmingIcon from '../../assets/swimming-icon.svg'
+import cyclingIcon from '../../assets/cycling-icon.svg'
+import weightliftingIcon from '../../assets/weightlifting-icon.svg'
+import './VerticalNavigation.css'
+
+function VerticalNavigation(){
+    return (
+      <div className="verticalNavContainer">
+        <nav className="verticalNav">
+          <ul>
+            <li>
+              <img
+                className="verticlIcon"
+                src={meditationIcon}
+                alt="meditaiton icon"
+              />
+            </li>
+            <li>
+              <img
+                className="verticlIcon"
+                src={swimmingIcon}
+                alt="swimming icon"
+              />
+            </li>
+            <li>
+              <img
+                className="verticlIcon"
+                src={cyclingIcon}
+                alt="cycling icon"
+              />
+            </li>
+            <li>
+              <img
+                className="verticlIcon"
+                src={weightliftingIcon}
+                alt="weightlifting icon"
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+}
+export default VerticalNavigation
