45a8ebd23c259c5cde113c284e30a222d230bcb8
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-02-02 15:58:20 +0100

    mock api update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,21 @@
 import { USER_MAIN_DATA, USER_AVERAGE_SESSIONS, USER_PERFORMANCE, USER_ACTIVITY } from "../__mocks__/mock_data";
 
-export const getUserData = () => {
+export const getUserMainData = () => {
     // next time => fetch to API instead of mock data
     return USER_MAIN_DATA[0];
+};
+
+export const getUserAverageSessions = () => {
+    // next time => fetch to API instead of mock data
+    return USER_AVERAGE_SESSIONS[0];
+}
+
+export const getUserPerformance = () => {
+    // next time => fetch to API instead of mock data
+    return USER_PERFORMANCE[0];
+}
+
+export const getUserActivity = () => {
+    // next time => fetch to API instead of mock data
+    return USER_ACTIVITY[0];
 }
