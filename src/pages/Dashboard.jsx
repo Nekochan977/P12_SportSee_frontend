8240b6c4af95ab390f12e135e157ab71bdf9c80f
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-02-03 09:53:51 +0100

    import user activity component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,22 @@
 import Navigation from "../components/navigations/Navigation"
 import VerticalNavigation from "../components/navigations/VerticalNavigation"
-// import VerticalNavigation from "./navigations/VerticalNavigation"
 import DashboardHeader from "../components/DashboardHeader"
+import UserActivity from "../components/UserActivity"
 import "./Dashboard.css"
 
 function Dashboard() {
-    return (
-      <div>
-        <Navigation />
-        <main className="DashboardContainer">
-            <VerticalNavigation />
-            <section className="dashboardContent">
-                <DashboardHeader />
-            </section>
-        </main>
-      </div>
-    );
-  }
-  
-  export default Dashboard;
+  return (
+    <div>
+      <Navigation />
+      <main className="DashboardContainer">
+        <VerticalNavigation />
+        <section className="dashboardContent">
+          <DashboardHeader />
+          <UserActivity />
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default Dashboard
