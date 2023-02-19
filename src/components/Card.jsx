7f7a20fb0e982560377c6156acc9f72952a6003f
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-02-19 11:49:13 +0100

    Icon cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+
+function Card({icon, quantity, unit, nutrimentType}){
+
+    return(
+        <div className="card-container">
+            <div className="card-item">
+                <div className="card-icon">
+                    <img src={icon} alt="icon"/>
+                </div>
+                <div className="card-text-content">
+                    <h3 className="key-data-name">{quantity} {unit}</h3>
+                    <p className="key-data-value">{nutrimentType}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,7 +6,14 @@ import UserAverageSession from "../components/AverageSession"
 import "./Dashboard.css"
 import UserPerformance from "../components/UserPerformance"
 import Score from "../components/Score";
-
+import Card from "../components/Card";
+import { getUserMainData } from "../utils/api"
+import Calories from "../assets/calories-icon.svg"
+import Carbs from "../assets/carbs-icon.svg"
+import Protein from "../assets/protein-icon.svg"
+import Fat from "../assets/fat-icon.svg"
+const userMainData = getUserMainData()
+console.log(userMainData.keyData.calorieCount)
 
 function Dashboard() {
     return (
@@ -24,6 +31,26 @@ function Dashboard() {
                             <Score/>
                         </section>
                         <aside>
+                            <Card
+                                icon={Calories}
+                                quantity={userMainData.keyData.calorieCount} unit="Kcal"
+                                nutrimentType="Calories"
+                            />
+                            <Card
+                                icon={Protein}
+                                quantity={userMainData.keyData.proteinCount} unit="g"
+                                nutrimentType="Protein"
+                            />
+                            <Card
+                                icon={Carbs}
+                                quantity={userMainData.keyData.carbohydrateCount} unit="g"
+                                nutrimentType="Glucides"
+                            />
+                            <Card
+                                icon={Fat}
+                                quantity={userMainData.keyData.lipidCount} unit="g"
+                                nutrimentType="Lipides"
+                            />
                         </aside>
                     </div>
 
