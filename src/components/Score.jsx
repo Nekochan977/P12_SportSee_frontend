c90bfe6de0612d563daf37bd08277ab21501141a
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-02-20 14:35:34 +0100

    pie chart look & field

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -14,22 +14,33 @@ function Score(){
     return(
         <div className="score-container">
             <h3 className="score-title">Score</h3>
-            <ResponsiveContainer width="100%" height="100%">
-                <PieChart width={400} height={400}>
+            <ResponsiveContainer
+                width="100%"
+                height="100%"
+            >
+                <PieChart
+                    width={400}
+                    height={400}
+                >
+
                     <Pie
                         data={score}
                         dataKey="value"
-                        outerRadius={70}
+                        outerRadius={90}
                         fill="#FFF"
+                        cx={150}
+                        cy={110}
                     />
                     <Pie
                         data={score}
                         dataKey="value"
-                        innerRadius={70}
-                        outerRadius={80}
+                        innerRadius={90}
+                        outerRadius={100}
                         startAngle={90}
                         endAngle={180}
                         fill="#ff0000"
+                        cx={150}
+                        cy={110}
                     />
                 </PieChart>
             </ResponsiveContainer>
