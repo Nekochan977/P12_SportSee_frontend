26d7bdc3b8e62296e543392b26f3e66e8cd885e7
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-02-28 12:33:00 +0100

    UserActivity look and feel

--- src/components/UserActivity.jsx
+++ src/components/UserActivity.jsx
@@ -1,4 +1,4 @@
-import { getUserActivity } from "../utils/api"
+import {getUserActivity} from "../utils/api"
 import {
     BarChart,
     Bar,
@@ -10,13 +10,22 @@ import {
     ResponsiveContainer
 } from "recharts"
 import "./UserActivity.css"
+import {logDOM} from "@testing-library/react";
 
 function UserActivity() {
     const userActivity = getUserActivity()
     const data = userActivity.sessions
+    // console.log(data)
 
-    const CustomToolTip = ({active, payload}) =>{
-        if (active){
+    // let day = data.map((el)=>{
+    //     let slice = el.day.slice(8)
+    //     let parsed = parseInt(slice)
+    //     return parsed
+    // })
+    // console.log(day)
+
+    const CustomToolTip = ({active, payload}) => {
+        if (active) {
             return (
                 <div className="custom-tooltip">
                     <p>{`${payload[0].value}`} kg</p>
@@ -26,69 +35,78 @@ function UserActivity() {
         }
         return null
     }
+
     return (
-      <div className="barchart">
-          <h2 className='barchartTitle'>Activité quotidienne</h2>
-          <ResponsiveContainer width="100%" height="100%">
-              <BarChart
-                  width={500}
-                  height={300}
-                  data={data}
-                  margin={{
-                      top: 5,
-                      right: 30,
-                      left: 30,
-                      bottom: 5,
-                  }}
-                  // barGap={8}
-              >
-                  <CartesianGrid
-                      strokeDasharray="1 1"
-                      vertical={false} />
-                  <XAxis
-                      dataKey="day"
-                      tickLine={false}
-                      tick={{fontSize: 14}}
-                      dy={15}
-                      stroke="1 1"
-                  />
-                  <YAxis
-                      orientation="right"
-                      tickCount="4"
-                      axisLine={false}
-                      tickLine={false}
-                      tick={{fontSize: 14}}
-                  />
-                  <Tooltip
-                      content={CustomToolTip}
-                  />
-                  <Legend
-                      verticalAlign="top"
-                      align="center"
-                      iconType="circle"
-                      margin={{
-                          top: 5,
-                          right: 30,
-                          left: 20,
-                          bottom: 30 }}
-                      height={36}/>
-                  <Bar
-                      dataKey="kilogram"
-                      fill="#282D30"
-                      radius={[20, 20, 0, 0]}
-                      maxBarSize={10}
-                      barCategoryGap={0}
-                  />
-                  <Bar
-                      dataKey="calories"
-                      fill="#E60000"
-                      radius={[20, 20, 0, 0]}
-                      maxBarSize={10}
-                      barCategoryGap={0}
-                      />
-              </BarChart>
-          </ResponsiveContainer>
-      </div>
-  )
+        <div className="barchart">
+            <div className="barchart-header">
+                <div className="barchart-titletitle">
+                    <h2>Activité quotidienne</h2>
+                </div>
+                <div className="barchart-icons">
+                    <div className="weight">
+                        <div className="black"></div>
+                        <p className="header-text"> Poids (kg)</p></div>
+                    <div className="calories">
+                        <div className="red"></div>
+                        <p className="header-text"> Calories brûlées (Kcal)</p></div>
+                </div>
+            </div>
+            <ResponsiveContainer width="100%" height="100%">
+                <BarChart
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 29,
+                        left: 43,
+                        bottom: 20,
+                    }}
+                >
+                    <CartesianGrid
+                        strokeDasharray="1 1"
+                        vertical={false}/>
+
+                    <XAxis
+                        dataKey="day"
+                        tickLine={false}
+                        tick={{fontSize: 14}}
+                        dy={15}
+                        stroke="#9B9EAC"
+                        padding={{
+                            top: 0,
+                            right: -10,
+                            left: -10,
+                            bottom: 0,
+                        }}
+                    />
+                    <YAxis
+                        orientation="right"
+                        tickCount="4"
+                        axisLine={false}
+                        tickLine={false}
+                        tick={{fontSize: 14}}
+                        stroke="#9B9EAC"
+                    />
+                    <Tooltip
+                        content={CustomToolTip}
+                    />
+                    <Bar
+                        dataKey="kilogram"
+                        fill="#282D30"
+                        radius={[20, 20, 0, 0]}
+                        maxBarSize={10}
+                        barCategoryGap={0}
+                    />
+                    <Bar
+                        dataKey="calories"
+                        fill="#E60000"
+                        radius={[20, 20, 0, 0]}
+                        maxBarSize={10}
+                        barCategoryGap={0}
+                    />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    )
 }
+
 export default UserActivity
