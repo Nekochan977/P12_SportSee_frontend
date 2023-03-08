344ee332365631058dd137a237abe5a4ab379646
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-03-08 17:10:46 +0100

    title, CustomTooltip, CustomHover, activeDot

--- src/components/AverageSession.jsx
+++ src/components/AverageSession.jsx
@@ -1,19 +1,19 @@
 import {getUserAverageSessions} from "../utils/api"
-import { LineChart, Line, XAxis, CartesianGrid, Legend } from "recharts"
+import {LineChart, Line, XAxis, Tooltip} from "recharts"
 import "./AverageSession.css"
 import {useEffect, useState} from "react";
 
 function UserAverageSession() {
-  // const averageSession = getUserAverageSessions()
-  // const data = averageSession.sessions
+    // const averageSession = getUserAverageSessions()
+    // const data = averageSession.sessions
     const [userAverageSession, setUserAverageSession] = useState(null)
 
     useEffect(() => {
-        const getData = async() => {
+        const getData = async () => {
             const result = await getUserAverageSessions()
             const response = await result.json()
             setUserAverageSession(response.data)
-            // console.log(response.data)
+            console.log(response.data)
         }
         getData()
     }, [])
@@ -22,75 +22,112 @@ function UserAverageSession() {
         return null
     }
 
-let day = userAverageSession.sessions.map((el)=>{
+    let day = userAverageSession.sessions.map((el) => {
 
-    switch (el.day) {
-        case 1 :
-            el.day = "L"
-        break
-        case 2:
-            el.day = "M"
-        break
-        case 3:
-            el.day = "M"
-        break
-        case 4:
-            el.day = "J"
-        break
-        case 5:
-            el.day = "V"
-        break
-        case 6:
-            el.day = "S"
-        break
-        case 7:
-            el.day = "D"
-            break
-        default:
-            break
-    }
+        switch (el.day) {
+            case 1 :
+                el.day = "L"
+                break
+            case 2:
+                el.day = "M"
+                break
+            case 3:
+                el.day = "M"
+                break
+            case 4:
+                el.day = "J"
+                break
+            case 5:
+                el.day = "V"
+                break
+            case 6:
+                el.day = "S"
+                break
+            case 7:
+                el.day = "D"
+                break
+            default:
+                break
+        }
         return el.day
 
-})
+    })
     return (
-    <>
-        <div className="line-chart-container">
-            {/*<CartesianGrid strokeDasharray="3 3" fill="#FF0000"/>*/}
-            <LineChart
-                width={200}
-                height={220}
-                margin={{
-                    top: 60,
-                    left: 0,
-                    right: 0,
-                    bottom: 40,
-                }}
-                data={userAverageSession.sessions}>
-                {/*<Legend/>*/}
-                <XAxis dataKey="day"
-                       height={20}
-                       tickLine={true}
-                       padding={{ left: 0, right: 5 }}
-                       axisLine={false}
-                       interval={0}
-                       stroke="red"
-                       dy={10}
-                       style={{ transform: "scale(0.93)", transformOrigin: "bottom" }}
-                       // margin={{top:15, right:5, left:5}}
-                       tick={{ fontSize: 13, stroke: "#FFFF", opacity: 0.5, margin: 5}} />
-                <Line
-                    type="monotone"
-                    dataKey="sessionLength"
-                    stroke="#FFF"
-                    strokeWidth={2}
-                    activeDot={{ r: 5 }}
-                    dot={false}
-                />
-            </LineChart>
-        </div>
-
-    </>
-  )
+        <>
+            <div className="line-chart-container">
+                <h2 className="line-chart-title">Durée moyenne des sessions</h2>
+                <LineChart
+                    width={250}
+                    height={220}
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        padding: "5px"
+                    }}
+                    data={userAverageSession.sessions}>
+                    <XAxis
+                        dataKey="day"
+                        height={20}
+                        tickLine={true}
+                        padding={{left: 5, right: 5}}
+                        axisLine={false}
+                        interval={0}
+                        stroke="red"
+                        dy={5}
+                        dx={2}
+                        tick={{
+                            fontSize: 13,
+                            stroke: "#FFFF",
+                            opacity: 0.5,
+                            margin: 5
+                    }}
+                    />
+                    <Tooltip
+                        content={<CustomToolTip/>}
+                        cursor={<CustomHover/>}
+                    />
+                    <Line
+                        type="monotone"
+                        dataKey="sessionLength"
+                        stroke="#FFF"
+                        strokeWidth={2}
+                        activeDot={{
+                            background: "#FFF",
+                            stroke: "rgba(255, 255, 255, 0.3)",
+                            strokeWidth: 10,
+                            r: 5,
+                        }}
+                        dot={false}
+                    />
+                </LineChart>
+            </div>
+        </>
+    )
 }
+
+function CustomToolTip({active, payload}) {
+    if (active) {
+        return (
+            <div className="custom-tooltip">
+                <p>{`${payload[0].value}`} min</p>
+            </div>
+        );
+    }
+    return null;
+}
+
+const CustomHover = ({points}) => {
+    return (
+        <rect
+            x={points[0].x}
+            y={0}
+            rx="5"
+            ry="5"
+            height="100%"
+            width="100%"
+            fill="rgba(0, 0, 0, 0.1)"
+        />
+    );
+};
 
 export default UserAverageSession
