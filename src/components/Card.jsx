298368b04edecf2d9fbbfcfa6791843b44982efc
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2023-03-10 16:20:32 +0100

    comments + correction on typo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,14 @@
 import "./Card.css"
 import PropTypes from "prop-types"
-function Card({icon, quantity, unit, nutrimentType}){
+/**
+ * Card
+ * @param {string} icon for nutrient type
+ * @param {number} quantity number of calories, proteins, carbohydrates or lipids
+ * @param {string} unit in g or kg
+ * @param {string} nutrientType calorie, protein, carbohydrate or lipid
+ * @returns card used for calorie, protein, carbohydrate and lipid
+ */
+function Card({icon, quantity, unit, nutrientType}){
 
     return(
         <div className="card-container">
@@ -10,7 +18,7 @@ function Card({icon, quantity, unit, nutrimentType}){
                 </div>
                 <div className="card-text-content">
                     <h3 className="key-data-name">{quantity} {unit}</h3>
-                    <p className="key-data-value">{nutrimentType}</p>
+                    <p className="key-data-value">{nutrientType}</p>
                 </div>
             </div>
         </div>
@@ -21,7 +29,7 @@ Card.propTypes = {
     icon: PropTypes.string,
     quantity: PropTypes.number,
     unit: PropTypes.string,
-    nutrimentType: PropTypes.string,
+    nutrientType: PropTypes.string,
 }
 
 export default Card
